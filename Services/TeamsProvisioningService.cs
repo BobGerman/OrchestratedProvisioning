fc8f043fdc604cd9596ca37e5bdafe6446138c58
Bob German <bgerman1@hotmail.com>
2019-10-22 21:34:07 -0400

    Almost creates team but need to get guid for user

--- Services/TeamsProvisioningService.cs
+++ Services/TeamsProvisioningService.cs
@@ -8,6 +8,9 @@ using OfficeDevPnP.Core.Framework.Graph;
 using Microsoft.Identity.Client;
 using System.Configuration;
 using System.Security;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
 
 namespace OrchestratedProvisioning.Services
 {
@@ -30,10 +33,45 @@ namespace OrchestratedProvisioning.Services
                 var tokenService = new TokenService(app);
                 var token = await tokenService.AcquireATokenFromCacheOrUsernamePasswordAsync(scopes, userName, password);
 
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Authorization =
+                        new AuthenticationHeaderValue("Bearer", token.AccessToken);
 
+                    var body = new StringContent(templateString);
+                    body.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    var response = await client.PostAsync(
+                        "https://graph.microsoft.com/beta/teams", body);
 
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        var responseContent = await response.Content.ReadAsStringAsync();
+                        var responseJson = JObject.Parse(responseContent);
+                        var errorMessage = responseJson["error"]["message"].ToString();
 
-                message.description = token.AccessToken;
+                        throw new Exception(errorMessage);
+                    }
+
+                    response.EnsureSuccessStatusCode();
+
+                    var operationUrl = response.Headers.Location;
+                    var done = false;
+                    HttpResponseMessage opResponse = null;
+                    while (!done)
+                    {
+                        await Task.Delay(5000);
+
+                        opResponse = await client.GetAsync(operationUrl);
+                        opResponse.EnsureSuccessStatusCode();
+
+                        done = opResponse.Headers.GetValues("status")?.FirstOrDefault<string>() == "succeeded";
+                    }
+
+                    string teamId = opResponse?.Headers.GetValues("id")?.FirstOrDefault<string>();
+                    //string resultContent = await response.Content.ReadAsStringAsync();
+
+                    message.description = teamId;
+                }
 
             }
 
